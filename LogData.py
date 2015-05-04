1883dccb44142047ad05b11396bc5c82dc265c17
Andrew M <milichab@gmail.com>
2015-05-04 13:42:45 -0400

    Fixed errors

--- LogData.py
+++ LogData.py
@@ -20,9 +20,9 @@ sensor = BMP085.BMP085()
 # bmp = BMP085(0x77, 2)  # HIRES Mode
 # bmp = BMP085(0x77, 3)  # ULTRAHIRES Mode
  
-temp = bmp.readTemperature()
-pressure = bmp.readPressure()
-altitude = bmp.readAltitude()
+temp = sensor.readTemperature()
+pressure = sensor.readPressure()
+altitude = sensor.readAltitude()
  
 print "Temperature: %.2f C" % temp
 print "Pressure:    %.2f hPa" % (pressure / 100.0)

1ac6a551e97f297a80a9a0cc661259e97bed7c2a
Andrew M <milichab@gmail.com>
2015-05-06 22:26:03 -0400

    Cleaned up file, redid printing.

--- LogData.py
+++ LogData.py
@@ -13,51 +13,77 @@ Revision 1.0: 5/4/15: Basic datalogging to print pressure, alt, temp and time.
 Revision 1.2: 5/5/15: Added GPS logging 
     TODO: add accelerometer data at the time. 
 
+Revision 1.3: 5/6/15: New logging 
+    TODO: test GPS 
+
+GitHub Repo: 
+    https://www.github.com/amilich/Bloons
+
 """
+
+# Log data: http://www.instructables.com/id/Raspberry-Pi-Temperature-Logger/
+# GPS tutorial: https://learn.adafruit.com/adafruit-ultimate-gps-on-the-raspberry-pi/using-your-gps
+
+printData = True 
 
 if __name__ == '__main__':
     #Balloon monitoring/logging data app 
     
+    #initialize sensors
     sensor = BMP085.BMP085(mode=BMP085.BMP085_ULTRAHIGHRES)
     lsm = Adafruit_LSM303()
     session = gps.gps("localhost", "2947")
     session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
-    
-    # log data: http://www.instructables.com/id/Raspberry-Pi-Temperature-Logger/
-
     print("Balloon Data Logger\n")
-    
-    readingNum = 1
-    
+    myFile = open("datalog.txt", "a")
+    myFile.write("Initializing Data Logger")
+
+    #if we don't have the current time, we can estimate it based on the number of readings and 
+    #the python time module 
+    readingNum = 0
+    start_time = time.time()
+
     while True:
         readingNum += 1
 
+        #update temperature readings 
         temp = sensor.read_temperature()
         pressure = sensor.read_pressure()
         altitude = sensor.read_altitude()
+
+        with open("datalog.txt", "a") as myFile:
+            #add reading # and then temperature data 
+            myFile.write("****** READING #" + readingNum + " ******")
+            myFile.write("****** TIME ELAPSED: " + (time.time() - start_time) + " ******")
+            tempLine =  "Temperature: %.2f C" % temp + " Pressure: %.2f hPa" % (pressure / 100.0) + "Altitude: %.2f" % altitude + "\n"
+            if printData: 
+                    print tempLine 
+            myFile.write(outstring)
+
+        if printData: 
+            print '[(Accelerometer X, Y, Z), (Magnetometer X, Y, Z, orientation)]'
+            print lsm.read()
+        myFile.write("(Accel X,Y,Z), (Magnetometer X, Y, Z, orient)")
+        myFile.write(lsm.read())
+
+        #gps data
         try: 
-            report = session.next()
-            print report 
-            #if report['class'] == 'TPV':
-            #    if hasattr(report, 'time'):
-            #        print report.time
+            report = session.next() #update GPS 
+            if printData: 
+                print report 
+            myFile.write("GPS Report: ")
+            myFile.write(report)
         except KeyError:
             pass
         except KeyboardInterrupt:
-            quit()
+            quit() #TODO: check this 
         except StopIteration:
             session = None
-            print "GPSD has terminated"
-        with open("datalog.txt", "a") as myFile:
-            now = datetime.datetime.now()
-            #add all data together
-            outvalue =  "Temperature: %.2f C" % temp + " Pressure:    %.2f hPa" % (pressure / 100.0) + "Altitude:    %.2f" % altitude + "\n"
-            outstring = "Reading #: " + "%i" % readingNum + ": " + str(outvalue)
-            print outstring
-            myFile.write(outstring)
-        print '[(Accelerometer X, Y, Z), (Magnetometer X, Y, Z, orientation)]'
-        print lsm.read()
-        #log temperature every 60 seconds
-        time.sleep(10)
+            if printData: 
+                print "GPSD has terminated"
+            myFile.write("*** GPS FAIL ***")
+
+        myFile.write("\n") #clean data with newline 
+        time.sleep(10) #sleep for 10s
     
